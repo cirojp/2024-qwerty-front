eeaaf1b9caa25c664c6bb3a105771735f1282366
Marcos Marcello <marcosmarcello17@gmail.com>
2024-09-03 15:27:20 -0300

    Added Transactions table and Login Style

--- repository/src/components/HomePage.jsx
+++ repository/src/components/HomePage.jsx
@@ -1,10 +1,40 @@
 import React, { useEffect, useState } from 'react'
-
+import DataTable from 'react-data-table-component'
 
 function HomePage() {
     const [transacciones, setTransacciones] = useState([]);
 
-    useEffect(() => {
+    const columns = [
+        {
+            name: "Motivo",
+            selector: row => row.motivo,
+        },
+        {
+            name: "Valor",
+            selector: row => row.valor,
+            sortable: true
+        }, 
+        {
+            name: "Fecha",
+            selector: row => row.fecha,
+            sortable: true
+        }
+    ]
+
+    const data = [
+        {
+            motivo: "Compras",
+            valor: 150,
+            fecha: "Ayer"
+        },
+        {
+            motivo: "Comida",
+            valor: 1500,
+            fecha: "Hoy"
+        }
+    ]
+
+    /*useEffect(() => {
         fetch("http://localhost:8080/api/transacciones", { method: "GET" })
             .then(response => response.json())
             .then(data => {
@@ -12,18 +42,30 @@ function HomePage() {
                 setTransacciones(data);
             })
             .catch(err => console.log(err));
-    }, []);
+    }, []);*/
 
     return (
         <div>
             <h1>Transacciones</h1>
+            <div>
+                <DataTable 
+                    title="Historial de Transacciones"
+                    columns={columns}
+                    data={data}
+                    pagination
+                />
+            </div>
+            {/*<div>
+            <h1>Transacciones</h1>
             <ul>
-                {transacciones.map(transaccion => (
+                {/*transacciones.map(transaccion => (
                     <li key={transaccion.id}>
                         Motivo: {transaccion.motivo}, Valor: {transaccion.valor}
                     </li>
                 ))}
+                <li></li>
             </ul>
+        </div>*/}
         </div>
     );
 }
--- repository/src/components/LoginForm.jsx
+++ repository/src/components/LoginForm.jsx
@@ -5,45 +5,73 @@ function LoginForm() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const onClick = ()=>{
+  
+  const onClick = () => {
     const userData = {
       username: username,
       password: password
-    }
+    };
     console.log(userData);
     setUsername("");
     setPassword("");
-    navigate("/index")
-  }
+    navigate("/index");
+  };
+
   const changeUsername = (event) => {
     setUsername(event.target.value);
-  }
+  };
+
   const changePassword = (event) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
+
   return (
-    <div className='container bg-success mw-100 mh-100 p-3'>
-      <div className="row ml-3 mr-3 border border-secondary rounded bg-dark">
-        <div className="col blockquote text-center text-light font-weight-bold">LOGIN</div>
-        <div className="w-100"/>
-        <div className="col blockquote text-center text-light">Ingrese su usuario y contraseña</div>
-        <div className="w-100"/>
-        <form className="bg-dark-subtle col">
+    <div className='min-h-screen flex items-center justify-center bg-green-500 p-4 h-full'>
+      <div className="w-full max-w-md bg-success border border-gray-700 rounded-lg p-6 shadow-lg">
+        <div className="text-center text-2xl text-black font-bold mb-4">LOGIN</div>
+        <div className="text-left text-black mb-3 ml-3">Ingrese su usuario y contraseña</div>
+        <form className="space-y-10">
           <div>
-            <label className="form-label text-light text-left">Usuario:</label>
-            <input type="text" className="form-control" value={username} placeholder='Username' onChange={(newEvent) => {changeUsername(newEvent)}}/><br/>
-            <label className="form-label text-light text-left">Contraseña:</label>
-            <input type="password" className="form-control" value={password} placeholder='Password' onChange={(newEvent) => {changePassword(newEvent)}}/><br/>
-            <div className="btn-group" role="group">
-              <input type="button" className="btn btn-lg btn-primary mb-3" value="Log In" onClick={onClick}/>
-              <input type="button" className="btn btn-lg btn-primary mb-3 ml-1" value="Sign Up" onClick={()=> {console.log("A Registrarse!")}}/>
-            </div>
+            <label className="block text-black mb-5 ml-3 mr-3">Usuario:</label>
+            <input 
+              type="text" 
+              className="w-50 ml-4 p-2 rounded bg-gray-700 border border-gray-600 placeholder-gray-400 text-white" 
+              value={username} 
+              placeholder='Username' 
+              onChange={changeUsername} 
+            />
+          </div>
+          <div>
+            <label className="block text-black ml-3 mb-5">Contraseña:</label>
+            <input 
+              type="password" 
+              className="w-50 ml-3 p-2 rounded bg-gray-700 border border-gray-600 placeholder-gray-400 text-white" 
+              value={password} 
+              placeholder='Password' 
+              onChange={changePassword} 
+            />
+          </div>
+          <div className="flex space-x-4 ml-3 ">
+            <button 
+              type="button" 
+              className="bg-primary text-white py-2 rounded hover:bg-blue-700"
+              onClick={onClick}
+            >
+              Log In
+            </button>
+            <button 
+              type="button" 
+              className="bg-primary text-white py-2 rounded hover:bg-blue-700"
+              onClick={() => { console.log("A Registrarse!"); }}
+            >
+              Sign Up
+            </button>
           </div>
         </form>
+        <div className='text-center text-gray-400 mt-4'>Recuperar Contraseña</div>
       </div>
-      <div className='text-center font-weight-light'>Recuperar Contraseña</div>
     </div>
-    );
+  );
 }
 
 export default LoginForm;

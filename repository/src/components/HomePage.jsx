6e1fbb88815cf41880903f3936ef0952f8a424ec
jp ciro <jciro@itba.edu.ar>
2024-09-02 22:31:05 -0300

    get de api y mostrar en pantalla

--- repository/src/components/HomePage.jsx
+++ repository/src/components/HomePage.jsx
@@ -1,18 +1,31 @@
 import React, { useEffect, useState } from 'react'
 
+
 function HomePage() {
-    const [transacciones, setTransacciones] = useState([])
-    useEffect(()=>{
-        fetch("http://localhost:8080/api/transacciones", {method: "GET", mode: 'no-cors'})
-        .then((data) => {
-            console.log(data);
-            setTransacciones(data)
-        })
-        .catch(err => console.log(err))
-    },[])
+    const [transacciones, setTransacciones] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/transacciones", { method: "GET" })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setTransacciones(data);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
     return (
-        <div>HomePage</div>
-    )
+        <div>
+            <h1>Transacciones</h1>
+            <ul>
+                {transacciones.map(transaccion => (
+                    <li key={transaccion.id}>
+                        Motivo: {transaccion.motivo}, Valor: {transaccion.valor}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default HomePage
+export default HomePage;

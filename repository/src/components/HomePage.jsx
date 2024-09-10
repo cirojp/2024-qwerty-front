da3fc349353d5e382bb000fcadd08ea6414ca6d7
jp ciro <jciro@itba.edu.ar>
2024-09-10 15:56:54 -0300

    modal para agregar transaccion

--- repository/src/components/HomePage.jsx
+++ repository/src/components/HomePage.jsx
@@ -3,6 +3,7 @@ import DataTable from 'react-data-table-component';
 import { useNavigate } from 'react-router-dom'; 
 import ExpandedRow from './ExpandedRow';
 import CreatableSelect from 'react-select/creatable';
+import Modal from 'react-modal';
 
 function HomePage() {
     const [transacciones, setTransacciones] = useState([]);
@@ -20,8 +21,36 @@ function HomePage() {
     ]);
     const [selectedPayMethod, setSelectedPayMethod] = useState(null); // Inicialmente nulo
     const navigate = useNavigate(); // Inicializa useNavigate para navegar entre rutas
-
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [transaccionId, setTransaccionId] = useState(null);
+
+    // Estilos para el modal
+    const modalStyles = {
+        content: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor: 'white',
+        padding: '20px',
+        borderRadius: '10px',
+        width: '400px',
+        maxWidth: '90%',
+        zIndex: 1001,
+        },
+        overlay: {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.7)', // Superposición oscura para cubrir el fondo
+            zIndex: 1000, // Asegura que la superposición esté por debajo del modal
+        },
+    };    
 
     // Validaciones
     const validateMotivo = (value) => {
@@ -132,6 +161,20 @@ function HomePage() {
         }
     };
 
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => {
+        setIsModalOpen(false);
+        clearForm();
+    };
+
+    const clearForm = () => {
+        setMotivo("");
+        setDescripcion("");
+        setValor("");
+        setSelectedPayMethod(null);
+        setFecha("");
+    };
+
     const editRow = (row) => {
         setEdit(true);
         setMotivo(row.motivo);
@@ -177,13 +220,7 @@ function HomePage() {
                     updatedTransacciones.sort((a, b) => new Date(b.fecha) - new Date(a.fecha)); // Orden descendente por fecha
                     setTransacciones(updatedTransacciones);
                 }
-                setMotivo("");
-                setDescripcion("");
-                setValor("");
-                setTipoGasto("");
-                setSelectedPayMethod(null);
-                setFecha("");
-                setEdit(false);
+                closeModal();
             } else {
                 setError("Error al agregar o actualizar la transacción");
             }
@@ -293,65 +330,88 @@ function HomePage() {
                     expandableRowsComponent={({data}) => <ExpandedRow data={data}/>}
                 />
             </div>
-            <h2 className="text-2xl font-semibold mb-4">{edit ? "Editar Transacción" : "Agregar Nueva Transacción"}</h2>
-            <form onSubmit={agregarTransaccion} className="space-y-4">
-                <div className="flex flex-col">
-                    <label className="mb-2 font-semibold">Motivo:</label>
-                    <input 
-                        type="text" 
-                        value={motivo}
-                        onChange={handleMotivoChange}
-                        className="border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                </div>
-                <div className="flex flex-col">
-                    <label className="mb-2 font-semibold">Descripcion:</label>
-                    <input 
-                        type="text" 
-                        value={descripcion}
-                        onChange={handleDescripcionChange}
-                        className="border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                </div>
-                <div className="flex flex-col">
-                    <label className="mb-2 font-semibold">Valor:</label>
-                    <input 
-                        type="number" 
-                        value={valor}
-                        onChange={(e) => setValor(e.target.value)}
-                        className="border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                </div>
-                <div className="flex flex-col">
-                    <label className="mb-2 font-semibold">Tipo de Gasto:</label>
-                    <CreatableSelect 
-                        options={payOptions} 
-                        onChange={handlePayChange}
-                        onCreateOption={handleCreate}
-                        value={selectedPayMethod}
-                    />
-                </div>
-                <div className="flex flex-col">
-                    <label className="mb-2 font-semibold">Fecha:</label>
-                    <input 
-                        type="datetime-local" 
-                        value={fecha}
-                        onChange={(e) => setFecha(e.target.value)}
-                        className="border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                </div>
+            <button 
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg"
+                onClick={openModal}
+            >
+                Agregar Transacción
+            </button>
+            <Modal
+                isOpen={isModalOpen}
+                onRequestClose={closeModal}
+                contentLabel="Agregar Transacción"
+                className="modal"
+                overlayClassName="modal-overlay"
+                style={{
+                    content: modalStyles.content,
+                    overlay: modalStyles.overlay,
+                }}
+            >
+                <h2 className="text-2xl font-semibold mb-4">{edit ? "Editar Transacción" : "Agregar Nueva Transacción"}</h2>
+                <form onSubmit={agregarTransaccion} className="space-y-4">
+                    <div className="flex flex-col">
+                        <label className="mb-2 font-semibold">Motivo:</label>
+                        <input 
+                            type="text" 
+                            value={motivo}
+                            onChange={handleMotivoChange}
+                            className="border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500"
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="mb-2 font-semibold">Descripcion:</label>
+                        <input 
+                            type="text" 
+                            value={descripcion}
+                            onChange={handleDescripcionChange}
+                            className="border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500"
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="mb-2 font-semibold">Valor:</label>
+                        <input 
+                            type="number" 
+                            value={valor}
+                            onChange={(e) => setValor(e.target.value)}
+                            className="border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500"
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="mb-2 font-semibold">Tipo de Gasto:</label>
+                        <CreatableSelect 
+                            options={payOptions} 
+                            onChange={handlePayChange}
+                            onCreateOption={handleCreate}
+                            value={selectedPayMethod}
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="mb-2 font-semibold">Fecha:</label>
+                        <input 
+                            type="datetime-local" 
+                            value={fecha}
+                            onChange={(e) => setFecha(e.target.value)}
+                            className="border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500"
+                            required
+                        />
+                    </div>
+                    <button 
+                        type="submit"
+                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg"
+                    >
+                        {edit ? "Guardar Cambios" : "Agregar Transacción"}
+                    </button>
+                </form>
                 <button 
-                    type="submit"
-                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg"
+                    onClick={closeModal}
+                    className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg"
                 >
-                    {edit ? "Guardar Cambios" : "Agregar Transacción"}
+                    Cerrar
                 </button>
-            </form>
-            {error && <p className="text-red-500 mt-4">{error}</p>}
+            </Modal>
             <div className="mt-6">
                 <button 
                     className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-lg"

77f8fdc8ded4632551874a29ab96380510f53c78
Marcos Marcello <marcosmarcello17@gmail.com>
2024-09-11 15:21:18 -0300

    Adapt buttons HomePage for Mobile View

--- repository/src/components/ActionButtons.jsx
+++ repository/src/components/ActionButtons.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 
 function ActionButtons({ navigate, signOff, deleteAccount }) {
     return (
-        <div className="mt-6">
+        <div className="mt-5">
             <button 
                 className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-lg"
                 onClick={() => navigate('/change-password')}
             >
                 Cambiar Contraseña
             </button>
+            <br />
             <button 
-                className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-lg ml-5"
+                className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-lg mt-3"
                 onClick={() => signOff()}
             >
                 Cerrar Sesion
             </button>
+            <br/>
             <button 
-                className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-lg ml-5"
+                className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-lg mt-3"
                 onClick={() => deleteAccount()}
             >
                 Eliminar Cuenta

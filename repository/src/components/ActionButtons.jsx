d756c8d60fbe07326caf0fa7b7dae0be9639dce4
Marcos Marcello <marcosmarcello17@gmail.com>
2024-09-12 19:26:59 -0300

    Fixed ActionButtons

--- repository/src/components/ActionButtons.jsx
+++ repository/src/components/ActionButtons.jsx
@@ -3,27 +3,27 @@ import "./styles/ActionButtons.css";
 
 function ActionButtons({ navigate, signOff, deleteAccount }) {
     return (
-        <div className="mt-5 flex justify-end space-x-4"> {/* Flex para alinear y espacio entre botones */}
+        <div className="mt-5 flex flex-col md:flex-row justify-end space-y-4 md:space-y-0 md:space-x-4">
+            {/* Cambiar a flex-col en pantallas pequeñas y mantener el espacio entre los botones */}
             <button 
-                className="w-auto bg-red-500 bg-opacity-80 text-white text-sm py-1 px-4 rounded-lg hover:bg-red-700"
+                className="w-full md:w-auto bg-red-500 bg-opacity-80 text-white text-sm py-2 px-4 rounded-lg hover:bg-red-700"
                 onClick={() => navigate('/change-password')}
             >
                 Cambiar Contraseña
             </button>
             <button 
-                className="w-auto bg-red-500 bg-opacity-80 text-white text-sm py-1 px-4 rounded-lg hover:bg-red-700"
+                className="w-full md:w-auto bg-red-500 bg-opacity-80 text-white text-sm py-2 px-4 rounded-lg hover:bg-red-700"
                 onClick={() => signOff()}
             >
                 Cerrar Sesión
             </button>
             <button 
-                className="w-auto bg-red-500 bg-opacity-80 text-white text-sm py-1 px-4 rounded-lg hover:bg-red-700"
+                className="w-full md:w-auto bg-red-500 bg-opacity-80 text-white text-sm py-2 px-4 rounded-lg hover:bg-red-700"
                 onClick={() => deleteAccount()}
             >
                 Eliminar Cuenta
             </button>
         </div>
-
     );
 }
 

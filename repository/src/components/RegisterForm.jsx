1a79aaa3063aca5a58452404345f95e472f2cad2
jp ciro <jciro@itba.edu.ar>
2024-09-22 17:36:44 -0300

    me faltaba flex

--- repository/src/components/RegisterForm.jsx
+++ repository/src/components/RegisterForm.jsx
@@ -112,7 +112,7 @@ function RegisterForm() {
           {error && <div className="text-red-500 text-sm text-center">{error}</div>}
             <button
               type="submit"
-              className="w-full bg-yellow-500 bg-opacity-80 text-gray-950 py-2 px-4 rounded-lg hover:bg-yellow-700flex justify-center items-center"
+              className="w-full bg-yellow-500 bg-opacity-80 text-gray-950 py-2 px-4 rounded-lg hover:bg-yellow-700 flex justify-center items-center"
               disabled={loading}
             >
               {loading ? (

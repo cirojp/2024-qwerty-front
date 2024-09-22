7af6b6627479c3ae0565d17f619cee42f8bee8b2
jp ciro <jciro@itba.edu.ar>
2024-09-22 16:40:03 -0300

    arreglar spinner en cambiar clave

--- repository/src/components/ChangePasswordForm.jsx
+++ repository/src/components/ChangePasswordForm.jsx
@@ -140,13 +140,10 @@ function ChangePasswordForm() {
                         disabled={loading}
                     >
                        {loading ? (
-                            <span className="flex items-center justify-center">
-                                <svg className="animate-spin h-5 w-5 mr-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                    <path className="opacity-75" fill="currentColor" d="M4 12c0-1.1.9-2 2-2h12c1.1 0 2 .9 2 2s-.9 2-2 2H6c-1.1 0-2-.9-2-2z"></path>
-                                </svg>
+                            <>
+                                <div className="loading-circle border-4 border-t-yellow-600 border-gray-200 rounded-full w-6 h-6 animate-spin mr-2"></div>
                                 Cargando...
-                            </span>
+                            </>
                         ) : (
                             "Cambiar Contraseña"
                         )}

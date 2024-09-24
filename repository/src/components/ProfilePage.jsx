a80e505fb5ed9719a85cfc81bf3c2c52a8bfa089
jp ciro <jciro@itba.edu.ar>
2024-09-24 12:21:47 -0300

    rollback

--- repository/src/components/ProfilePage.jsx
+++ repository/src/components/ProfilePage.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react'
 import ActionButtons from './ActionButtons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBlender, faCandyCane, faFileInvoiceDollar, faHouse, faTicket } from '@fortawesome/free-solid-svg-icons';
-import icon1 from '../assets/iconosCategorias/icon1.png';
-import icon2 from '../assets/iconosCategorias/icon2.png';
 
 function ProfilePage() {
     const defaultCategories = [
@@ -15,10 +13,6 @@ function ProfilePage() {
     ];
 
     const [payCategories, setPayCategories] = useState(defaultCategories); 
-    const iconMap = {
-        'icon1.png': icon1,
-        'icon2.png': icon2,
-    };
 
     const fetchPersonalCategorias = async () => {
         const token = localStorage.getItem("token");
@@ -50,7 +44,8 @@ function ProfilePage() {
                 <ul>
                     {payCategories.map((category) => (
                         <li key={category.value} className="flex items-center mb-2">
-                            <img src={iconMap[category.iconPath]} alt={category.label} className="w-6 h-6 mr-2" />
+                            <img src={`../assets/iconosCategorias/${category.iconPath}`} alt={category.label} className="w-6 h-6 mr-2" />
+
                             {category.label}
                         </li>
                     ))}

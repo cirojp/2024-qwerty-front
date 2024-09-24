d23faa181fd6d2f5aade24b3883b2fc1be24578e
jp ciro <jciro@itba.edu.ar>
2024-09-24 10:33:28 -0300

    visualisar cats

--- repository/src/components/ProfilePage.jsx
+++ repository/src/components/ProfilePage.jsx
@@ -4,13 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBlender, faCandyCane, faFileInvoiceDollar, faHouse, faTicket } from '@fortawesome/free-solid-svg-icons';
 
 function ProfilePage() {
-    const [payCategories, setPayCategories] = useState([
-        {value: "Impuestos y Servicios", label: "Impuestos y Servicios"},
-        {value: "Entretenimiento y Ocio", label: "Entretenimiento y Ocio"},
-        {value: "Hogar y Mercado", label: "Hogar y Mercado"},
-        {value: "Antojos", label: "Antojos"},
-        {value: "Electro", label: "Electrodomesticos"},
-      ]);
+    const defaultCategories = [
+        { value: "Impuestos y Servicios", label: "Impuestos y Servicios" },
+        { value: "Entretenimiento y Ocio", label: "Entretenimiento y Ocio" },
+        { value: "Hogar y Mercado", label: "Hogar y Mercado" },
+        { value: "Antojos", label: "Antojos" },
+        { value: "Electro", label: "Electrodomesticos" }
+    ];
+
+    const [payCategories, setPayCategories] = useState(defaultCategories); 
+
     const fetchPersonalCategorias = async () => {
         const token = localStorage.getItem("token");
         try {
@@ -23,7 +26,7 @@ function ProfilePage() {
             if (response.ok) {
                 const data = await response.json();
                 const customOptions = data.map(cat => ({ label: cat.nombre, value: cat.nombre }));
-                setPayCategories([...payCategories, ...customOptions]);
+                setPayCategories(prevCategories => [...prevCategories, ...customOptions]);
             }
         } catch (error) {
             console.error("Error al obtener las categorías personalizadas:", error);
@@ -50,13 +53,12 @@ function ProfilePage() {
     </div>*/
     <div className="container min-h-screen min-w-full max-w-full bg-black">
         <div className='text-white font-bold'>Mi Cuenta</div>
-        <ActionButtons/>
+        <ActionButtons />
         <div className='text-white'>
             <div className='text-bold text-yellow-500 text-xl mb-3 underline'>Mis Categorias</div>
             <ul>
                 {payCategories.map((category) => (
                     <li key={category.value}>
-                        
                         {category.value}
                     </li>
                 ))}
@@ -66,4 +68,4 @@ function ProfilePage() {
   )
 }
 
-export default ProfilePage
+export default ProfilePage;

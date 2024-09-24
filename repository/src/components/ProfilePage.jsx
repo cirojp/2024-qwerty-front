07a16dcfdf83a21eed4f2c0fc823af96bc3aaa6f
jp ciro <jciro@itba.edu.ar>
2024-09-24 13:22:10 -0300

    toma imagen de carpeta?

--- repository/src/components/ProfilePage.jsx
+++ repository/src/components/ProfilePage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import ActionButtons from './ActionButtons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBlender, faCandyCane, faFileInvoiceDollar, faHouse, faTicket } from '@fortawesome/free-solid-svg-icons';
-import icono from "../assets/edit-icon.png";
+import icono from "../assets/iconosCategorias/edit-icon.png";
 //import icono2 from '../assets/iconosCategorias/icono2.png';
 
 

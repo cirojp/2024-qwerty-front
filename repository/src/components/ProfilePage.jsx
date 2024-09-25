fe08acbec23cd366412f3b91905cfe9511a3d419
Marcos Marcello <marcosmarcello17@gmail.com>
2024-09-25 14:06:05 -0300

    Added edit/delete buttons

--- repository/src/components/ProfilePage.jsx
+++ repository/src/components/ProfilePage.jsx
@@ -1,13 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import ActionButtons from './ActionButtons'
+import React, { useEffect, useState } from 'react';
+import ActionButtons from './ActionButtons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core';
 
-
-/*
-    No se porque busca dos veces las categorias, y si actualizas sigue sumando
- */
 function ProfilePage() {
     library.add(fas);
     const defaultCategories = [
@@ -17,7 +13,8 @@ function ProfilePage() {
         { value: "Antojos", label: "Antojos", iconPath: "fa-solid fa-candy-cane", textColor: 'mr-2 text-yellow-500'},
         { value: "Electrodomesticos", label: "Electrodomesticos", iconPath: "fa-solid fa-blender", textColor: 'mr-2 text-yellow-500'}
     ];
-    const [payCategories, setPayCategories] = useState(defaultCategories); 
+    
+    const [payCategories, setPayCategories] = useState(defaultCategories);
 
     const fetchPersonalCategorias = async () => {
         const token = localStorage.getItem("token");
@@ -29,33 +26,90 @@ function ProfilePage() {
             });
             if (response.ok) {
                 const data = await response.json();
-                const customOptions = data.map(cat => ({ label: cat.nombre, value: cat.nombre, iconPath: cat.iconPath, textColor: 'mr-2 text-white'}));
-                setPayCategories(prevCategories => [...prevCategories, ...customOptions]);
+                const customOptions = data.map(cat => ({ label: cat.nombre, value: cat.nombre, iconPath: cat.iconPath, textColor: 'mr-2 text-white' }));
+                setPayCategories(customOptions);
             }
         } catch (error) {
             console.error("Error al obtener las categorías personalizadas:", error);
         }
     };
+
     useEffect(() => {
         fetchPersonalCategorias();
     }, []);
-  return (
+
+    const handleEdit = (categoryValue) => {
+        console.log(`Editar categoría: ${categoryValue}`);
+    };
+
+    // Función para eliminar una categoría
+    const handleDelete = async(categoryValue) => {
+        const filteredCategories = payCategories.filter(category => category.value == categoryValue);
+        const token = localStorage.getItem("token");
+        console.log(token);
+        const nombre =  filteredCategories[0].label;
+        const iconPath =  filteredCategories[0].iconPath;
+        try {
+            const response = await fetch("https://two024-qwerty-back-2.onrender.com/api/personal-categoria", {
+                method: "DELETE",
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                },
+                body: JSON.stringify({nombre, iconPath})
+            });
+            if(response.ok){
+                setPayCategories([]);
+                fetchPersonalCategorias();
+                console.log(`Categoría eliminada: ${categoryValue}`);
+            }
+        }catch(err){
+            throw new Error();
+        }
+    };
+
+    return (
         <div className="container min-h-screen min-w-full max-w-full bg-black">
             <div className='text-white font-bold'>Mi Cuenta</div>
             <ActionButtons />
             <div className='text-white'>
                 <div className='text-bold text-yellow-500 text-xl mb-3 underline'>Mis Categorias</div>
                 <ul>
+                    {defaultCategories.map((category) => (
+                        <li key={category.value} className="flex items-center justify-between mb-2">
+                            <div className="flex items-center">
+                                <FontAwesomeIcon icon={category.iconPath} className={category.textColor} />
+                                <div className={category.textColor}>{category.label}</div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <ul>
                     {payCategories.map((category) => (
-                        <li key={category.value} className="flex items-center mb-2">
-                            <FontAwesomeIcon icon={category.iconPath} className={category.textColor} />
-                            <div className={category.textColor}>{category.label}</div>
+                        <li key={category.value} className="flex items-center justify-between mb-2">
+                            <div className="flex items-center">
+                                <FontAwesomeIcon icon={category.iconPath} className={category.textColor} />
+                                <div className={category.textColor}>{category.label}</div>
+                            </div>
+                            <div>
+                                <button 
+                                    className="ml-4 text-blue-500 hover:text-blue-700"
+                                    onClick={() => handleEdit(category.value)}
+                                >
+                                    Editar
+                                </button>
+                                <button 
+                                    className="ml-2 text-red-500 hover:text-red-700"
+                                    onClick={() => handleDelete(category.value)}
+                                >
+                                    X
+                                </button>
+                            </div>
                         </li>
                     ))}
                 </ul>
             </div>
         </div>
-  )
+    );
 }
 
 export default ProfilePage;

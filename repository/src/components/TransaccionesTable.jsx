f97b185321a9941ed65f909ffcf9324d5958626d
jp ciro <jciro@itba.edu.ar>
2024-09-27 01:31:01 -0300

    sin hasMounted?

--- repository/src/components/TransaccionesTable.jsx
+++ repository/src/components/TransaccionesTable.jsx
@@ -17,7 +17,7 @@ function TransaccionesTable({ transacciones, editRow, deleteRow, onTableEmpty = 
         
     });
     library.add(fas);
-    const [hasMounted, setHasMounted] = useState(false);
+    //const [hasMounted, setHasMounted] = useState(false);
     useEffect(() => {
         // Solo ejecutar después del primer renderizado
         if (hasMounted) {
@@ -27,7 +27,7 @@ function TransaccionesTable({ transacciones, editRow, deleteRow, onTableEmpty = 
             // Marcar que el componente ha sido montado
             setHasMounted(true);
         }
-    }, [payCategories, hasMounted]);
+    }, [payCategories]);
 
     const columns = [
         {

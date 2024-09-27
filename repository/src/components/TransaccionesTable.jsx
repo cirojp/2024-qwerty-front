ae6c89acc642728326dde681a1b235715cec95d4
jp ciro <jciro@itba.edu.ar>
2024-09-27 00:45:48 -0300

    reRenderizar transacciones al crear categoria o eliminarla

--- repository/src/components/TransaccionesTable.jsx
+++ repository/src/components/TransaccionesTable.jsx
@@ -17,6 +17,8 @@ function TransaccionesTable({ transacciones, editRow, deleteRow, onTableEmpty = 
         
     });
     library.add(fas);
+    useEffect(() => {
+    }, [payCategories]);
 
     const columns = [
         {

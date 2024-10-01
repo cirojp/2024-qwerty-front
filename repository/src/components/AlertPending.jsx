0760ea3691295f368abd323db92353b37155c07b
Marcos Marcello <marcosmarcello17@gmail.com>
2024-10-01 19:40:23 -0300

    Add Loading to Accept Transaction

--- repository/src/components/AlertPending.jsx
+++ repository/src/components/AlertPending.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 
 function AlertPending({pendingTransaction, isOpen, isAccepted = () => {}, isRejected = () => {}}) {
@@ -28,9 +28,12 @@ function AlertPending({pendingTransaction, isOpen, isAccepted = () => {}, isReje
             overflowY: 'auto', // Habilitamos scroll si el contenido es demasiado grande
         },
     };
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleAccept = () => {
+        setIsLoading(true);
         isAccepted(pendingTransaction);
+        setIsLoading(false);
     }
 
     const handleReject = () => {
@@ -52,8 +55,18 @@ function AlertPending({pendingTransaction, isOpen, isAccepted = () => {}, isReje
             <br/>
             <label>Fecha: {pendingTransaction.fecha}</label>
             <br/>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded mr-2" onClick={handleAccept}>Aceptar</button>
-            <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded" onClick={handleReject}>Rechazar</button>
+            <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-3 rounded mr-2" onClick={handleAccept} disabled={isLoading}>
+            {isLoading ? (
+                            <div className="flex items-center justify-center">
+                                <svg className="animate-spin h-5 w-5 text-gray-950" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
+                                </svg>
+                                <span className="ml-2">Cargando...</span>
+                            </div>
+                        ) : "Aceptar"}
+                </button>
+            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-3 rounded" onClick={handleReject}>Rechazar</button>
         </Modal>
     )
 }

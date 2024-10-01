231505c8832068fa9e03a067770ca99591761eef
Marcos Marcello <marcosmarcello17@gmail.com>
2024-10-01 14:29:31 -0300

    Undo check token

--- repository/src/components/PrivateRoutes.jsx
+++ repository/src/components/PrivateRoutes.jsx
@@ -1,39 +1,16 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-const checkIfValidToken = async (token) => {
-    try {
-        const response = await fetch("https://two024-qwerty-back-2.onrender.com/api/transacciones/userTest", {
-            headers: {
-                "Authorization": `Bearer ${token}`
-            }
-        });
-        if (response.ok) {
-            return token;
-        }else{
-            localStorage.removeItem("token");
-            return null;
-        }
-    } catch (error) {
-        console.error("Error al obtener las categorías personalizadas:", error);
-        localStorage.removeItem("token");
-        return null;
-    }
-};
+const PrivateRoutes = () => {
+    const token = localStorage.getItem('token'); // Verifica si el token existe
 
-const PrivateRoutes = async() => {
-    
-    const actualToken = localStorage.getItem("token");
-    const token = checkIfValidToken(actualToken);
-
+    // Si no hay token, redirige al login
     if (!token) {
         return <Navigate to="/" />;
     }
-    
-    return <Outlet />;
-    
 
     // Si hay token, renderiza el componente hijo (la página protegida)
+    return <Outlet />;
 };
 
 export default PrivateRoutes;

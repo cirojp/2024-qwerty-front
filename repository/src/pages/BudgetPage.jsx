af59909e8b4b425f321893663ef5e130d54fb0f3
Marcos Marcello <marcosmarcello17@gmail.com>
2024-10-28 16:51:33 -0300

    Add GetBudgets

--- repository/src/pages/BudgetPage.jsx
+++ repository/src/pages/BudgetPage.jsx
@@ -1,13 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BudgetCard from "./components/BudgetCard";
 import ModalCreateBudget from "./components/ModalCreateBudget";
 
 function BudgetPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [presupuestos, setPresupuestos] = useState([]);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
   const defaultIcon = "https://cdn-icons-png.freepik.com/256/781/781760.png";
+  const getPersonalPresupuestos = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch(
+        "https://two024-qwerty-back-2.onrender.com/api/presupuesto",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setPresupuestos(data);
+      }
+    } catch (error) {
+      console.error("Error al obtener las categorías personalizadas:", error);
+    }
+  };
+  useEffect(() => {
+    getPersonalPresupuestos();
+  }, []);
   return (
     <div className="container h-full mx-auto p-4 bg-black">
       <div className="text-2xl font-semibold text-center mb-4 text-white">
@@ -19,26 +43,21 @@ function BudgetPage() {
         </button>
       </div>
       <div className="flex flex-col gap-6">
-        <BudgetCard
-          title="Restaurantes"
-          icon={defaultIcon}
-          dateFrom="01/10/2024"
-          dateTo="31/10/2024"
-          percentage={46}
-          currentAmount="45,50 SAR"
-          maxAmount="100,00 SAR"
-          residualAmount="54,50 SAR"
-        />
-        <BudgetCard
-          title="Combustible"
-          icon={defaultIcon}
-          dateFrom="01/10/2024"
-          dateTo="31/10/2024"
-          percentage={25}
-          currentAmount="30,00 SAR"
-          maxAmount="120,00 SAR"
-          residualAmount="90,00 SAR"
-        />
+        {presupuestos.map((budget) => {
+          const categoryNames = Object.keys(budget.categoryBudgets).join(", ");
+          return (
+            <BudgetCard
+              title={categoryNames}
+              icon={defaultIcon}
+              dateFrom="01/10/2024"
+              dateTo="31/10/2024"
+              percentage={0}
+              currentAmount="$0"
+              maxAmount={"$" + budget.totalBudget}
+              residualAmount={"$" + budget.totalBudget}
+            />
+          );
+        })}
       </div>
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
